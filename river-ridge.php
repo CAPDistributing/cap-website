aba099398d69e9b6ef3d2aaed1874415b73dcc05
Belax8 <belax_8@hotmail.com>
2016-05-26 20:55:35 +0000

    added Audio Technica

--- river-ridge.php
+++ river-ridge.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html ng-app="store">
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>CAP Distributing - Angular.js</title>
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/main.css" />
+  
+	<title>Audio Technica - CAP Distributing</title>
+  <?php include_once("head.html"); ?>
+  
 </head>
 <body ng-controller="StoreController as store">
+  
+  <?php include_once("cap-logo.html"); ?>
+  <?php include_once("nav.html"); ?>
+  
   <div class="container">
-    <?php include_once("cap-logo.html"); ?>
-    <?php include_once("nav.html"); ?>
-    <h1>Audio Technica</h1>
+    <h2>River Ridge Wallets</h2>
 
-    <div ng-repeat="product in store.riverRidge">
-      <h2>{{product.name}}</h2>
-      <h3>{{product.price | currency}}</h3>
-      <p>{{product.description}}</p>
-      <ul>
-        <li>{{product.bullets[0]}}</li>
-        <li>{{product.bullets[1]}}</li>
+    <div class="col-md-4 product-info" ng-repeat="product in store.riverRidge" ng-hide="product.soldOut">
+      <img class="product-img" ng-src="{{product.image.src}}" />
+      <h3 class="product-name">{{product.name}}</h3>
+      <ul class="product-bullets-ul">
+        <li class="product-bullets-li">{{product.bullets[0]}}</li>
+        <li class="product-bullets-li">{{product.bullets[1]}}</li>
       </ul>
-      <img ng-src="{{product.image.src}}" />
-      <a class="buy-button" ng-hide="product.soldOut" ng-href="{{product.amazonLink}}">Buy on Amazon &gt;&gt;</a>
+      <a class="buy-button" ng-href="{{product.amazonLink}}">Buy on Amazon &gt;&gt;</a>
     </div>
 
-    <?php include_once("footer.html"); ?>
-  </div><!-- End .container -->
-
+  </div><?php #End .container ?>
+  
+  <?php include_once("footer.html"); ?>
+  
 </body>
 </html>
--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,138 @@
 
   var audioTechnica = [
     {
+      name: "AudioTechnica Active Noise-cancelling Headphones",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/anc70.jpg",
+  		    alt: "Black Headphones"
+  	  },
+      bullets: [
+        "Frequency response: 10-25,000 Hz",
+        "Quietpoint Active Noise Reduction: up to 20 dB",
+      ],
+      amazonLink: "http://www.amazon.com/gp/product/B00FJ1HFGC?m=A2THIGJ83VMAAL&qid=1458084379&ref_=sr_1_32&s=merchant-items&sr=1-32",
+      soldOut: true,
+      onSale: false
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear AX3iS (Black)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/AX3iSBK.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage",
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX3IS-SonicFuel-Over-Ear-Headphones/dp/B00JTBHCR0?ie=UTF8&m=A2THIGJ83VMAAL&qid=1459574341&ref_=sr_1_73&s=merchant-items&sr=1-73",
+      soldOut: false,
+      onSale: true
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear AX3iS (Red)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/AX3iSRD.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage",
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX3IS-SonicFuel-Over-Ear-Headphones/dp/B00JTBHCZM?ie=UTF8&m=A2THIGJ83VMAAL&qid=1459574341&ref_=sr_1_87&s=merchant-items&sr=1-87",
+      soldOut: false,
+      onSale: true
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear AX3iS (Navy)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/AX3iSBL.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage",
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX3IS-SonicFuel-Over-Ear-Headphones/dp/B00IF6WSLK?ie=UTF8&m=A2THIGJ83VMAAL&qid=1459574341&ref_=sr_1_85&s=merchant-items&sr=1-85",
+      soldOut: false,
+      onSale: true
+    },
+    {
+      name: "AudioTechnica Core 150 In-Ear Headphones (Pink)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/COR150PK.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Full, textured bass with immersive sound and excellent detail resolution",
+        "Easy-traveling audio performance with cord-wrap included",
+        "In-ear (canal-style) headphones"
+      ],
+      amazonLink: "http://www.amazon.com/Audio-Technica-ATHCOR150PK--Ear-Headphones/dp/B00BIDHA6U/ref=sr_1_1?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1462024387&sr=1-1&keywords=pink",
+      soldOut: false,
+      onSale: true
+    },
+    {
+      name: "AudioTechnica Clear 100 In-Ear Headphones (Lime Green)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/CLR100LG.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Crystal-clear sound and excellent detail resolution",
+        "Easy-traveling audio performance with cord-wrap included",
+        "In-ear (canal-style) headphones"
+      ],
+      amazonLink: "http://www.amazon.com/Audio-Technica-ATHCLR100LG-Headphones-Green/dp/B00BBDL3QK/ref=sr_1_3?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1462023767&sr=1-3&refinements=p_4%3AAudio-Technica",
+      soldOut: false,
+      onSale: true
+    },
+    {
+      name: "AudioTechnica Clear 100 In-Ear Headphones (Orange)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "/images/audio-technica/CLR100OR.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Crystal-clear sound and excellent detail resolution",
+        "Easy-traveling audio performance with cord-wrap included",
+        "In-ear (canal-style) headphones"
+      ],
+      amazonLink: "http://www.amazon.com/Audio-Technica-ATHCLR100OR-Headphones-Orange/dp/B00BBDKTS8/ref=sr_1_2?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1462023585&sr=1-2&keywords=orange",
+      soldOut: false,
+      onSale: true
+    },
+    {
       name: "AudioTechnica SonicFuel Over-Ear AX1iS (Black)",
   	  description: "These are black headphones",
   	  itemNum: "891-12614",
@@ -23,15 +155,47 @@
         "Durable construction for active, everyday use",
         "Earcups fold flat for easy storage"
       ],
-  	  specs: {
-    		height: "some inches",
-    		width: "some inches",
-    		depth: "some inches",
-    		weight: "some ounces"
-  	  },
       amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX1iS-SonicFuel-Over-Ear-Headphones/dp/B00JTBH7A2/ref=sr_1_5?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1450969239&sr=1-5",
       soldOut: false,
-      onSale: true
+      onSale: false
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear Headphones (White)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "images/audio-technica/AX1iSWH2.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage"
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX1iS-SonicFuel-Over-Ear-Headphones/dp/B00JTBH7M0/ref=sr_1_8?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1452304657&sr=1-8",
+      soldOut: false,
+      onSale: false
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear Headphones (Grey)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "images/audio-technica/AX1iSGY2.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage"
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX1iS-SonicFuel-Over-Ear-Headphones/dp/B00JTBH7IY/ref=sr_1_7?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1450970746&sr=1-7",
+      soldOut: false,
+      onSale: false
     },
     {
       name: "AudioTechnica SonicFuel Over-Ear AX1iS (Red)",
@@ -48,21 +212,34 @@
         "Durable construction for active, everyday use",
         "Earcups fold flat for easy storage"
       ],
-  	  specs: {
-    		height: "some inches",
-    		width: "some inches",
-    		depth: "some inches",
-    		weight: "some ounces"
-  	  },
       amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX1iS-SonicFuel-Over-Ear-Headphones/dp/B00JTBH7PC/ref=sr_1_20?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1450970746&sr=1-20",
       soldOut: false,
-      onSale: true
+      onSale: false
+    },
+    {
+      name: "AudioTechnica SonicFuel Over-Ear Headphones (Blue)",
+  	  description: "",
+  	  itemNum: "",
+  	  upc: "",
+      price: "",
+  	  image: {
+  		    src: "images/audio-technica/AX1iSBL2.jpg",
+  		    alt: ""
+  	  },
+      bullets: [
+        "Large 40 mm drivers deliver enhanced sound",
+        "Durable construction for active, everyday use",
+        "Earcups fold flat for easy storage"
+      ],
+      amazonLink: "http://www.amazon.com/AudioTechnica-ATH-AX1iS-SonicFuel-Over-Ear-Headphones/dp/B00JTBH7MU/ref=sr_1_16?m=A2THIGJ83VMAAL&s=merchant-items&ie=UTF8&qid=1450970746&sr=1-16",
+      soldOut: false,
+      onSale: false
     },
 
 
 
 
-  ]; // end var audioTechnica
+  ]; // end audioTechnica
 
   var riverRidge = [
     {
@@ -79,12 +256,6 @@
         'Size: Approx. 4" x 3"',
         'Inside Details: 8 Credit Card Inserts, 1 Window ID, 2 Bill Slots'
       ],
-  	  specs: {
-    		height: "some inches",
-    		width: "some inches",
-    		depth: "some inches",
-    		weight: "some ounces"
-  	  },
       amazonLink: "http://www.amazon.com/dp/B013IFATOK/ref=twister_B013Q5PIIO?_encoding=UTF8&psc=1",
       soldOut: false,
       onSale: true
@@ -93,6 +264,27 @@
 
 
 
-  ]; // end var riverRidge
+  ]; // end riverRidge
 
 })();
+
+
+/*
+  {
+    name: "",
+	  description: "",
+	  itemNum: "",
+	  upc: "",
+    price: "",
+	  image: {
+		    src: "",
+		    alt: ""
+	  },
+    bullets: [
+      ""
+    ],
+    amazonLink: "",
+    soldOut: false,
+    onSale: false
+  },
+*/
