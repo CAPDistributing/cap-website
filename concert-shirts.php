45b28136333b1785c09329bf24349d0db05aa9c6
Belax8 <belax_8@hotmail.com>
2016-06-14 14:58:42 +0000

    cleaned up concert shirts page

--- concert-shirts.php
+++ concert-shirts.php
@@ -15,13 +15,13 @@
     <h2>Concert Shirts</h2>
 
     <div class="col-md-4 product-info" ng-repeat="product in store.concertShirts" ng-hide="product.soldOut">
-      <img class="product-img" ng-src="{{product.image.src}}" />
+      <img class="product-img product-img-concert-shirts" ng-src="{{product.image.src}}" />
       <h3 class="product-name">{{product.name}}</h3>
       <ul class="product-bullets-ul">
         <li class="product-bullets-li">{{product.bullets[0]}}</li>
-        <li class="product-bullets-li">{{product.bullets[1]}}</li>
+        <li class="product-bullets-li">Availiable in multiple sizes</li>
       </ul>
-      <a class="buy-button" ng-href="{{product.amazonLink}}">Buy on Amazon &gt;&gt;</a>
+      <a class="btn btn-cap" ng-href="{{product.amazonLink}}">Buy on Amazon &gt;&gt;</a>
     </div>
 
   </div><?php #End .container ?>
