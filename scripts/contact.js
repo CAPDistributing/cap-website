c63bf1a0464f60f33c169d9e560a4bb87cf466fc
Chase <belax_8@hotmail.com>
2016-07-26 07:26:09 -0600

    cleaned up some code

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,12 +1,12 @@
 
 //variable
-var $el = $(".container");
-var $name = $el.find("#name");
-var $email = $el.find("#email");
-var $subject = $el.find("#subject");
-var $message = $el.find("#message");
-var $submit = $el.find("#submit");
-var $formStatus = $el.find(".form-status");
+var $el = $('.container');
+var $name = $el.find('#name');
+var $email = $el.find('#email');
+var $subject = $el.find('#subject');
+var $message = $el.find('#message');
+var $submit = $el.find('#submit');
+var $formStatus = $el.find('.form-status');
 
 
 //event listeners
@@ -20,25 +20,25 @@ function sendMessage() {
     email: $email.val(),
     subject: $subject.val(),
     message: $message.val(),
-    website: "capdistributing.com"
+    website: 'capdistributing.com'
   };
 
   $.ajax({
     type: "POST",
-    url: "http://rest.learncode.academy/api/aTjPn7InWo8BlqrR/mB2974BuosGlyUks",
+    url: 'http://rest.learncode.academy/api/aTjPn7InWo8BlqrR/mB2974BuosGlyUks',
     data: messageInfo,
     success: function(newMessage) {
       //console.log(newMessage);
       $formStatus.html('<div class="alert alert-success" role="alert">Your message has been sent!</div>');
     },
     error: function() {
-      alert("An error occurred while sending your message!");
+      alert('An error occurred while sending your message!');
     }
   });
 
-  $name.val("");
-  $email.val("");
-  $subject.val("");
-  $message.val("");
+  $name.val('');
+  $email.val('');
+  $subject.val('');
+  $message.val('');
 
 }
