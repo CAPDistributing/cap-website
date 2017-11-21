396b42902dcda50b619823ed4375c21019006ff7
Belax8 <belax_8@hotmail.com>
2017-11-21 14:18:48 +0000

    Moved our Terms of Use and Security Policy over to our website.

---
+++ security-policy.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html ng-app="store" lang="en">
+<head>
+	
+    <title>Security Policy - CAP Distributing</title>
+    <meta name="description" content="CAP Distributing's security policy." />
+    <meta name="keywords" content="security policy" />
+    <?php include_once("templates/head.html"); ?>
+	
+</head>
+<body>
+  
+    <?php include_once("templates/cap-logo.html"); ?>
+    <?php include_once("templates/nav.html"); ?>
+    <div class="container">
+        
+        <h2>Web Application Security Policy</h2>
+        
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>This Privacy Policy was last modified on July 16, 2017.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>CAP Distributing LLC ("us", "we", or "our") operates capdistributing.com (the "Site"). This page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from users of the Site.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>We use your Personal Information only for providing and improving the Site. By using the Site, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible at capdistributing.com.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Information Collection And Use</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your name, email address, postal address and phone number ("Personal Information").</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Log Data</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Site that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Cookies</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Site.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Security</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Links To Other Sites</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>Our Site may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>CAP Distributing LLC has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party sites or services.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Changes To This Privacy Policy</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>CAP Distributing LLC may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on the Site. You are advised to review this Privacy Policy periodically for any changes.</p>
+          </div>
+        </div>
+        
+        <h3 style="text-align: center;">Contact Us</h3>
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <p>If you have any questions about this Privacy Policy, please <a href="/contact.php">contact us</a>.</p>
+          </div>
+        </div>
+    
+    </div><!-- End .container -->
+    <?php include_once("templates/footer.html"); ?>
+    
+</body>
+</html>
